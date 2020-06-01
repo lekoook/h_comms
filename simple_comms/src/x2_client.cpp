f316194a6d2db1471d924dd4f2f3daf1f88028da
Tan Tze Guang <omnikron96@gmail.com>
2020-06-01 18:30:36 +0800

    Separating communications into 3 client nodes

---
+++ simple_comms/src/x2_client.cpp
@@ -0,0 +1,46 @@
+#include "ros/ros.h"
+#include "std_msgs/String.h"
+#include "subt_communication_broker/subt_communication_client.h"
+
+#include <sstream>
+
+void Recv(const std::string &_srcAddress, const std::string &_dstAddress, const uint32_t _dstPort, const std::string &_data);
+
+int main(int argc, char **argv)
+{
+    ros::init(argc, argv, "x2_comms");
+    ros::NodeHandle n;
+    ros::Rate loop_rate(0.5);
+
+    subt::CommsClient x2Client = subt::CommsClient("X2");
+
+    x2Client.Bind(&Recv, "X2");
+
+    while (ros::ok())
+    {
+        std::stringstream ss;
+        subt::CommsClient::Neighbor_M neigh;
+        neigh = x2Client.Neighbors();
+
+        x2Client.SendTo("I am X2", subt::communication_broker::kBroadcast);
+
+        ROS_INFO("-----------------------------------");
+        ROS_INFO("X2's Neighbours:");
+        for (subt::CommsClient::Neighbor_M::iterator it = neigh.begin(); it != neigh.end(); it++)
+        { 
+          ss << it->first << " : " << it->second.first << "," << it->second.second;
+          ROS_INFO("%s", ss.str().c_str());
+        }
+
+        ros::spinOnce();
+        loop_rate.sleep();
+    }
+    return 0;
+}
+
+void Recv(const std::string &_srcAddress, const std::string &_dstAddress, const uint32_t _dstPort, const std::string &_data)
+{
+    std::stringstream ss;
+    ss << "X2 received: " << _data << " - from " << _srcAddress;
+    ROS_INFO("%s", ss.str().c_str());
+}
---
+++ simple_comms/src/x3_client.cpp
@@ -0,0 +1,46 @@
+#include "ros/ros.h"
+#include "std_msgs/String.h"
+#include "subt_communication_broker/subt_communication_client.h"
+
+#include <sstream>
+
+void Recv(const std::string &_srcAddress, const std::string &_dstAddress, const uint32_t _dstPort, const std::string &_data);
+
+int main(int argc, char **argv)
+{
+    ros::init(argc, argv, "x3_comms");
+    ros::NodeHandle n;
+    ros::Rate loop_rate(0.5);
+
+    subt::CommsClient x3Client = subt::CommsClient("X3");
+
+    x3Client.Bind(&Recv, "X3");
+
+    while (ros::ok())
+    {
+        std::stringstream ss;
+        subt::CommsClient::Neighbor_M neigh;
+        neigh = x3Client.Neighbors();
+
+        x3Client.SendTo("I am X3", subt::communication_broker::kBroadcast);
+        
+        ROS_INFO("-----------------------------------");
+        ROS_INFO("X3's Neighbours:");
+        for (subt::CommsClient::Neighbor_M::iterator it = neigh.begin(); it != neigh.end(); it++)
+        { 
+          ss << it->first << " : " << it->second.first << "," << it->second.second;
+          ROS_INFO("%s", ss.str().c_str());
+        }
+
+        ros::spinOnce();
+        loop_rate.sleep();
+    }
+    return 0;
+}
+
+void Recv(const std::string &_srcAddress, const std::string &_dstAddress, const uint32_t _dstPort, const std::string &_data)
+{
+    std::stringstream ss;
+    ss << "X3 received: " << _data << " - from " << _srcAddress;
+    ROS_INFO("%s", ss.str().c_str());
+}
