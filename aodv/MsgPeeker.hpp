da4fc0624bb105245f8e08c509e17461fef52f6c
zhengqunkoo <root@zhengqunkoo.com>
2020-06-28 09:21:23 +0800

    Copy MsgPeeker.hpp from socket_transceiver

---
+++ aodv/MsgPeeker.hpp
@@ -0,0 +1,32 @@
+#ifndef MSG_PEEKER_H
+#define MSG_PEEKER_H
+
+#include <iostream>
+#include <stdint.h>
+#include "MsgTypes.hpp"
+#include "utilities/serialisers.hpp"
+
+namespace msg_peeker
+{
+    aodv_msgs::MsgTypes peekType(uint8_t* msg)
+    {
+        uint8_t t = serialisers::getU8(msg);
+        switch (t)
+        {
+        case 1:
+            return aodv_msgs::MsgTypes::Rreq;
+        
+        case 2:
+            return aodv_msgs::MsgTypes::Rrep;
+
+        case 3:
+            return aodv_msgs::MsgTypes::Rrer;
+
+        default:
+            std::cerr << "Unknown message type peeked." << std::endl;
+            return aodv_msgs::MsgTypes::Unknown; // TODO: What exactly to return here?
+        }
+    }
+}
+
+#endif // MSG_PEEKER_H
