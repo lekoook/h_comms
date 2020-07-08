0694f2c0afcb025e290861a9014e06421e1d81bc
zhengqunkoo <root@zhengqunkoo.com>
2020-07-08 20:11:38 +0800

    Fix multiple redefinition error
    Split file into {h,c}pp

---
+++ aodv/node/MsgPeeker.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <stdint.h>
+#include "MsgPeeker.hpp"
+#include "../utilities/serialisers.hpp"
+
+namespace msg_peeker
+{
+    aodv_msgs::MsgTypes peekType(uint8_t* msg)
+    {
+        uint8_t t = serialisers::getU8(msg);
+        switch (t)
+        {
+        case 1:
+            return aodv_msgs::MsgTypes::Rreq;
+        
+        case 2:
+            return aodv_msgs::MsgTypes::Rrep;
+
+        case 3:
+            return aodv_msgs::MsgTypes::Rerr;
+
+        case 4:
+            return aodv_msgs::MsgTypes::RrepAck;
+
+        default:
+            std::cerr << "Unknown message type peeked." << std::endl;
+            return aodv_msgs::MsgTypes::Unknown; // TODO: What exactly to return here?
+        }
+    }
+}
--- aodv/utilities/serialisers.hpp
+++ aodv/utilities/serialisers.hpp
@@ -15,10 +15,7 @@ namespace serialisers
      * @param dest uint8_t array to store the copied bytes. MUST be at least 1 byte length.
      * @param src uint8_t value to copy.
      */
-    void copyU8(uint8_t* dest, uint8_t src)
-    {
-        dest[0] = src;
-    }
+    void copyU8(uint8_t* dest, uint8_t src);
 
     /**
      * @brief Copies a uint16_t value into a uint8_t bytes array.
@@ -26,11 +23,7 @@ namespace serialisers
      * @param dest uint8_t array to store the copied bytes. MUST be at least 2 byte length.
      * @param src uint16_t value to copy.
      */
-    void copyU16(uint8_t* dest, uint16_t src)
-    {
-        dest[0] = (uint8_t) ((src >> 8) & 0xff);
-        dest[1] = (uint8_t) (src & 0xff);
-    }
+    void copyU16(uint8_t* dest, uint16_t src);
 
     /**
      * @brief Copies a uint32_t value into a uint8_t bytes array.
@@ -38,13 +31,7 @@ namespace serialisers
      * @param dest uint8_t array to store the copied bytes. MUST be at least 4 bytes length.
      * @param src uint32_t value to copy.
      */
-    void copyU32(uint8_t* dest, uint32_t src)
-    {
-        dest[0] = (uint8_t) ((src >> 24) & 0xff);
-        dest[1] = (uint8_t) ((src >> 16) & 0xff);
-        dest[2] = (uint8_t) ((src >> 8) & 0xff);
-        dest[3] = (uint8_t) (src & 0xff);
-    }
+    void copyU32(uint8_t* dest, uint32_t src);
 
     /**
      * @brief Retrieves a uint8_t value from a uint8_t bytes array.
@@ -52,10 +39,7 @@ namespace serialisers
      * @param src uint8_t array to retrieve from. MUST be at least 1 byte length.
      * @return uint8_t value retrieved.
      */
-    uint8_t getU8(uint8_t* src)
-    {
-        return src[0];
-    }
+    uint8_t getU8(uint8_t* src);
 
     /**
      * @brief Retrieves a uint16_t value from a uint8_t bytes array.
@@ -63,13 +47,7 @@ namespace serialisers
      * @param src uint8_t array to retrieve from. MUST be at least 1 byte length.
      * @return uint16_t value retrieved.
      */
-    uint16_t getU16(uint8_t* src)
-    {
-        uint16_t val = 0;
-        val |= (src[0] & 0xff) << 8;
-        val |= (src[1] & 0xff);
-        return val;
-    }
+    uint16_t getU16(uint8_t* src);
 
     /**
      * @brief Retrieves a uint32_t value from a uint8_t bytes array.
@@ -77,15 +55,7 @@ namespace serialisers
      * @param src uint8_t array to retrieve from. MUST be at least 4 bytes length.
      * @return uint32_t value retrieved.
      */
-    uint32_t getU32(uint8_t* src)
-    {
-        uint32_t val = 0;
-        val |= (src[0] & 0xff) << 24;
-        val |= (src[1] & 0xff) << 16;
-        val |= (src[2] & 0xff) << 8;
-        val |= (src[3] & 0xff);
-        return val;
-    }
+    uint32_t getU32(uint8_t* src);
 }
 
 #endif // SERIALISERS_H
