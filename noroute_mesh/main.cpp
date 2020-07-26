6b57e1c0ab273284f3f8ac4802c16eb4bca3f6c3
zhengqunkoo <root@zhengqunkoo.com>
2020-07-26 18:39:32 +0800

    Adapt parson to this branch, now does not segfault

--- noroute_mesh/main.cpp
+++ noroute_mesh/main.cpp
@@ -1,21 +1,44 @@
+#include "lib/parson.h"
 #include "utilities/Sim.hpp"
 
 int main() {
-  uint8_t numNodes = 2;
-  int numEvents;
+  JSON_Value *json_root = json_parse_file("config.json");
+  JSON_Array *json_configs = json_value_get_array(json_root);
 
-  numEvents = 2;
-  aodv::config_t *config1 = (aodv::config_t*)malloc(sizeof(*config1) + numEvents * sizeof(config1->events[0]));
-  config1->numEvents = numEvents;
-  config1->events[0] = {0, aodv::Eth()};
-  config1->events[1] = {1, aodv::Eth()};
+  size_t numNodes = json_array_get_count(json_configs);
+  size_t numEvents;
+  aodv::config_t configs[numNodes];
+  aodv::config_t *config;
+  int time;
+  uint32_t seq;
+  std::string dst, src;
+  uint8_t* payload;
+  uint16_t dstLength, srcLength, payloadLength;
 
-  numEvents = 1;
-  aodv::config_t *config2 = (aodv::config_t*)malloc(sizeof(*config2) + numEvents * sizeof(config2->events[0]));
-  config2->numEvents = numEvents;
-  config2->events[0] = {1, aodv::Eth()};
+  JSON_Array *json_config;
+  JSON_Object *json_event;
+  JSON_Object *json_eth;
+  for (size_t i=0; i<numNodes; i++) {
+    json_config = json_array_get_array(json_configs, i);
+    numEvents = json_array_get_count(json_config);
+    config = (aodv::config_t*)malloc(sizeof(*config) + numEvents * sizeof(config->events[0]));
+    config->numEvents = numEvents;
+    for (size_t j=0; j<numEvents; j++) {
+      json_event = json_array_get_object(json_config, j);
+      time = json_object_get_number(json_event, "time");
+      json_eth = json_object_get_object(json_event, "eth");
+      seq = json_object_get_number(json_eth, "seq");
+      dstLength = json_object_get_string_len(json_eth, "dst");
+      dst = json_object_get_string(json_eth, "dst");
+      srcLength = json_object_get_string_len(json_eth, "src");
+      src = json_object_get_string(json_eth, "src");
+      payloadLength = json_object_get_string_len(json_eth, "payload");
+      payload = (uint8_t*)json_object_get_string(json_eth, "payload");
+      config->events[j] = {time, aodv::Eth(seq, dstLength, dst, srcLength, src, payloadLength, payload)};
+    }
+    configs[i] = *config;
+  }
 
-  aodv::config_t configs[] = {*config1, *config2};
   aodv::Sim sim = aodv::Sim(numNodes, configs);
   sim.start();
 
