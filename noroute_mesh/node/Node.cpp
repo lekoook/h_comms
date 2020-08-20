95ecd094e5008125570e4d9a0e349165c4d05e14
zhengqunkoo <root@zhengqunkoo.com>
2020-08-20 20:23:22 +0800

    Partially revert Node::send
    From noroute_mesh-segmentation-test to noroute-mesh-devel

--- noroute_mesh/node/Node.cpp
+++ noroute_mesh/node/Node.cpp
@@ -22,16 +22,19 @@ namespace aodv
             this->seq++;
         }
 
+        const uint16_t inputPayloadLength = eth.payloadLength;
+
         // Where seg is a segment, aodv::MAX_MESSAGE_SIZE/2 == aodv::ETH_NONVAR_LEN + seg.srcLength + seg.dstLength + seg.payloadLength
         // Where eth is a packet, there are sizeof(typeof(eth.segSeqMax)) segments.
         // So the maximum eth.payloadLength must be:
         const uint16_t maxPayloadLength = aodv::MAX_MESSAGE_SIZE/2 - (aodv::ETH_NONVAR_LEN + eth.srcLength + eth.dstLength);
-        assert (eth.payloadLength <= sizeof(uint32_t) * maxPayloadLength);
+        uint32_t maxSegments;
         if (eth.payloadLength < maxPayloadLength) {
-            eth.segSeqMax = 1;
+            maxSegments = 1;
         } else {
-            eth.segSeqMax = (eth.payloadLength / maxPayloadLength) + ((eth.payloadLength % maxPayloadLength) != 0);
+            maxSegments = (eth.payloadLength / maxPayloadLength) + ((eth.payloadLength % maxPayloadLength) != 0);
         }
+        eth.segSeqMax = maxSegments;
 
         std::vector<std::string> retval = std::vector<std::string>(eth.segSeqMax, "");
 
@@ -41,15 +44,33 @@ namespace aodv
         this->seq++;
 
         // Split packet into segments.
-        int segSeq=0, p=maxPayloadLength;
-        for (; p<eth.payloadLength; segSeq++,p+=maxPayloadLength) {
+        int pos=0;
+        for (int segSeq = 0; segSeq < maxSegments; segSeq++)
+        {
             aodv::Eth seg(eth);
-            seg.payloadLength = maxPayloadLength;
-            seg.payload = eth.payload.substr(p - maxPayloadLength, seg.payloadLength);
             seg.segSeq = segSeq;
 
-            uint16_t length = aodv::ETH_NONVAR_LEN + seg.srcLength + seg.dstLength + seg.payloadLength;
-            uint8_t msg[length];
+            // Figure out the length of this segment.
+            if (maxSegments == 1) // If we only have one segment, just use the input length.
+            {
+                seg.payloadLength = inputPayloadLength;
+            }
+            else if (segSeq == maxSegments - 1) // This is the last segment.
+            {
+                seg.payloadLength = inputPayloadLength - (maxPayloadLength * (maxSegments - 1));
+            }
+            else
+            {
+                seg.payloadLength = maxPayloadLength;
+            }
+
+            // Set the payload for this segment.
+            seg.payload = eth.payload.substr(pos, pos + seg.payloadLength);
+            pos += seg.payloadLength;
+
+            // Send segment.
+            uint16_t segLen = aodv::ETH_NONVAR_LEN + seg.srcLength + seg.dstLength + seg.payloadLength;
+            uint8_t msg[segLen] = { 0 };
             std::cout << "SENDING" << std::endl
                 // << seg.seq << std::endl
                 // << seg.dst << std::endl
@@ -59,27 +80,9 @@ namespace aodv
                 << seg.payloadLength << std::endl
                 << seg.payload << std::endl;
             seg.serialise(msg);
-            retval[segSeq] = uint8_to_string(msg, length);
+            retval[segSeq] = uint8_to_string(msg, segLen);
         }
 
-        aodv::Eth seg(eth);
-        seg.payloadLength = eth.payloadLength - (p - maxPayloadLength);
-        seg.payload = eth.payload.substr(p - maxPayloadLength, seg.payloadLength);
-        seg.segSeq = segSeq;
-
-        uint16_t length = aodv::ETH_NONVAR_LEN + eth.srcLength + eth.dstLength + eth.payloadLength;
-        uint8_t msg[length];
-
-        std::cout << "SENDING" << std::endl
-            // << seg.seq << std::endl
-            // << seg.dst << std::endl
-            // << seg.dstLength << std::endl
-            // << seg.src << std::endl
-            // << seg.srcLength << std::endl
-            << seg.payloadLength << std::endl
-            << seg.payload << std::endl;
-        seg.serialise(msg);
-        retval[segSeq] = uint8_to_string(msg, length);
         return retval;
     }
 
