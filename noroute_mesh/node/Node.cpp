3d9d7bc80fdfdb5401161191870ee55a437717cb
zhengqunkoo <root@zhengqunkoo.com>
2020-08-22 11:41:36 +0800

    Represent as string with escaped null char and escaped escape

--- noroute_mesh/node/Node.cpp
+++ noroute_mesh/node/Node.cpp
@@ -171,37 +171,36 @@ namespace aodv
 
     std::string Node::uint8_to_string(uint8_t b[], std::string::size_type l)
     {
-        /*
-         * A uint8_t has bits denoted as: abcdefgh.
-         */
-        char c;
-        uint8_t s[l*2];
-        std::string::size_type i=0;
-        for (; i<l; i++) {
-            c = 0b10101010u;
-            c |= (b[i] >> 7) << 6;
-            c |= ((b[i] >> 6) & 1) << 4;
-            c |= ((b[i] >> 5) & 1) << 2;
-            c |= (b[i] >> 4) & 1;
-            s[2*i] = c;
-            c = 0b10101010u;
-            c |= ((b[i] >> 3) & 1) << 6;
-            c |= ((b[i] >> 2) & 1) << 4;
-            c |= ((b[i] >> 1) & 1) << 2;
-            c |= b[i] & 1;
-            s[2*i+1] = c;
+        std::string s = "";
+        for (std::string::size_type i=0; i<l; i++) {
+            switch (b[i]) {
+            case '\0':
+                s += "\0";
+            case '\\':
+                s += "\\\\";
+            default:
+                s += b[i];
+            }
         }
-        return std::string((char*)s,l*2);
+        return s;
     }
 
     void Node::string_to_uint8(uint8_t b[], std::string s)
     {
-        /*
-         * Denotation of bits is as in the body of uint8_to_string(uint8_t b[], std::string::size_type l).
-         */
-        for (std::string::size_type i=0; i<s.size(); i+=2) {
-            b[i/2] = ((s[i] & 0b01000000u) << 1) | ((s[i] & 0b00010000u) << 2) | ((s[i] & 0b00000100u) << 3) | ((s[i] & 1) << 4);
-            b[i/2] |= ((s[i+1] & 0b01000000u) >> 3) | ((s[i+1] & 0b00010000u) >> 2) | ((s[i+1] & 0b00000100u) >> 1) | (s[i+1] & 1);
+        for (std::string::size_type i, j = 0; i<s.size(); ) {
+            if (s[i] == '\\' && s[i+1] == '0') {
+                b[j] = '\0';
+                i += 2;
+                j += 1;
+            if (s[i] == '\\' && s[i+1] == '\\') {
+                b[j] = '\\';
+                i += 2;
+                j += 1;
+            } else {
+                b[j] = s[i];
+                i += 1;
+                j += 1;
+            }
         }
     }
 }
