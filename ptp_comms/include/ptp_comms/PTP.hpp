638496f3986596c6e191d2a1668014843aa0badb
lekoook <kokteng1313@gmail.com>
2020-12-23 20:55:16 +0800

    Abstract out the exchange sequence into a class

---
+++ ptp_comms/include/ptp_comms/PTP.hpp
@@ -0,0 +1,141 @@
+#include <memory>
+#include "subt_communication_broker/subt_communication_client.h"
+#include "ptp_comms/TxData.h"
+#include "ptp_comms/RxData.h"
+#include "TxerTh.hpp"
+#include "RxerTh.hpp"
+
+/**
+ * @brief Class that encapsulates the point-to-point communication exchange sequences.
+ * 
+ */
+class PTP
+{
+private:
+    // Constants
+    /**
+     * @brief Interval (seconds) between each subt::CommsClient beacon broadcast.
+     * 
+     */
+    const double PING_INTERVAL = 1.0;
+
+    /**
+     * @brief Local address of this node.
+     * 
+     */
+    std::string nodeAddr;
+
+    /**
+     * @brief ROS publisher to publish received data.
+     * 
+     */
+    ros::Publisher rxPubber;
+
+    /**
+     * @brief ROS service server to respond to transmission requests.
+     * 
+     */
+    ros::ServiceServer txService;
+
+    /**
+     * @brief subt::CommsClient to transmit and receive data.
+     * 
+     */
+    std::unique_ptr<subt::CommsClient> cc;
+
+    /**
+     * @brief Transmission thread that will handle all messages to be transmitted.
+     * 
+     */
+    std::unique_ptr<TxerTh> txTh;
+
+    /**
+     * @brief Reception thread that will handle all received messages.
+     * 
+     */
+    std::unique_ptr<RxerTh> rxTh;
+
+    /**
+     * @brief Callback for receive data from subt::CommsClient.
+     * 
+     * @param _srcAddress Source address of the data.
+     * @param _dstAddress Destination address of the data.
+     * @param _dstPort Destination port of the data.
+     * @param _data Actual data received.
+     */
+    void _rxCb(const std::string &_srcAddress, const std::string &_dstAddress, const uint32_t _dstPort, const std::string &_data)
+    {
+        if (_srcAddress != nodeAddr)
+        {
+            while(!rxTh->recvOne(RxQueueData(_data, _srcAddress, _dstAddress)));
+        }
+    }
+
+    /**
+     * @brief Callback for transmission service request calls.
+     * 
+     * @param req Request message.
+     * @param res Response message.
+     * @return true If data was successfully queued.
+     * @return false If data was not queued.
+     */
+    bool _txData(ptp_comms::TxData::Request &req, ptp_comms::TxData::Response &res)
+    {
+        if (req.dest != nodeAddr)
+        {
+            res.successful = txTh->sendOne(req.data, req.dest);
+        }
+        else
+        {
+            res.successful = false;
+        }
+        
+        return res.successful;
+    }
+
+    /**
+     * @brief Callback that should be called to publish received data.
+     * 
+     * @param src Source address of the data.
+     * @param data Actual data received.
+     */
+    void _pubRx(std::string src, std::vector<uint8_t> data)
+    {
+        ptp_comms::RxData msg;
+        msg.data = data;
+        msg.src = src;
+        rxPubber.publish(msg);
+    }
+
+public:
+    /**
+     * @brief Construct a new PTP object.
+     * 
+     * @param nh ROS NodeHandle.
+     * @param nodeAddr Address for this node.
+     */
+    PTP(ros::NodeHandle& nh, std::string nodeAddr) : nodeAddr(nodeAddr)
+    {
+        // Set up ROS structures.
+        rxPubber = nh.advertise<ptp_comms::RxData>("rx_data", 100);
+
+        // Set up communication drivers.
+        cc = std::unique_ptr<subt::CommsClient>(new subt::CommsClient(nodeAddr));
+        txTh = std::unique_ptr<TxerTh>(new TxerTh(cc.get()));
+        rxTh = std::unique_ptr<RxerTh>(new RxerTh(
+            cc.get(), 
+            txTh.get(), 
+            std::bind(&PTP::_pubRx, this, std::placeholders::_1, std::placeholders::_2)));
+
+        cc->StartBeaconInterval(ros::Duration(PING_INTERVAL));
+        // Default bind
+        cc->Bind(std::bind(&PTP::_rxCb, this, std::placeholders::_1, std::placeholders::_2, std::placeholders::_3, std::placeholders::_4), nodeAddr);
+        txTh->start();
+        rxTh->start();
+
+        txService = nh.advertiseService<ptp_comms::TxData::Request, ptp_comms::TxData::Response>(
+            "tx_data", 
+            std::bind(&PTP::_txData, this, std::placeholders::_1, std::placeholders::_2)
+            );
+    }
+};
--- ptp_comms/src/ptp_comms_node.cpp
+++ ptp_comms/src/ptp_comms_node.cpp
@@ -1,69 +1,14 @@
 #include "ros/ros.h"
-#include "ptp_comms/TxData.h"
-#include "ptp_comms/RxData.h"
-#include "TxerTh.hpp"
-#include "RxerTh.hpp"
-
-RxerTh* pRxTh; // TODO: Is there a better way than using global variable?
-ros::Publisher rxPubber;
-std::string robotAddr;
-
-void rxCb(const std::string &_srcAddress, const std::string &_dstAddress, const uint32_t _dstPort, const std::string &_data)
-{
-    if (_srcAddress != robotAddr)
-    {
-        while(!pRxTh->recvOne(RxQueueData(_data, _srcAddress, _dstAddress)));
-    }
-}
-
-bool txData(ptp_comms::TxData::Request &req, ptp_comms::TxData::Response &res, TxerTh* th)
-{
-    if (req.dest != robotAddr)
-    {
-        res.successful = th->sendOne(req.data, req.dest);
-    }
-    else
-    {
-        res.successful = false;
-    }
-    
-    return res.successful;
-}
-
-void pubRx(std::string src, std::vector<uint8_t> data)
-{
-    ptp_comms::RxData msg;
-    msg.data = data;
-    msg.src = src;
-    rxPubber.publish(msg);
-}
+#include "PTP.hpp"
 
 int main(int argc, char** argv)
 {
-    // Constants
-    const double PING_INTERVAL = 1.0;
-
-    robotAddr = argv[1];
+    std::string robotAddr = argv[1];
 
     ros::init(argc, argv, "point_to_point_comms");
     ros::NodeHandle nh;
-    rxPubber = nh.advertise<ptp_comms::RxData>("rx_data", 100);
-    
-    subt::CommsClient cc(robotAddr);
-    TxerTh txTh(&cc);
-    std::function<void(std::string, std::vector<uint8_t>)> cb = &pubRx;
-    RxerTh rxTh(&cc, &txTh, pubRx);
-    pRxTh = &rxTh;
 
-    cc.Bind(rxCb, robotAddr);
-    cc.StartBeaconInterval(ros::Duration(PING_INTERVAL));
-    txTh.start();
-    rxTh.start();
-
-    ros::ServiceServer txService = nh.advertiseService<ptp_comms::TxData::Request, ptp_comms::TxData::Response>(
-        "tx_data", 
-        std::bind(&txData, std::placeholders::_1, std::placeholders::_2, &txTh)
-        );
+    PTP ptp(nh, robotAddr);
 
     ros::spin();
 }
