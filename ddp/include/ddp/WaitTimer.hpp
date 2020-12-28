abe0e6c35c0ddf54b9ab7f4bd26fb9ad8b033975
lekoook <kokteng1313@gmail.com>
2020-12-28 17:22:13 +0800

    Modify WaitTime

--- ddp/include/ddp/WaitTimer.hpp
+++ ddp/include/ddp/WaitTimer.hpp
@@ -4,17 +4,55 @@
 #include "ros/ros.h"
 #include <condition_variable>
 
+/**
+ * @brief Timer that allows caller to wait for a specific amount of simulation time (seconds) or interrupt the timer
+ * before it elapses.
+ * 
+ */
 class WaitTimer
 {
 private:
-    double waitTime;
+    /**
+     * @brief ROS timer used to wait in simulation time.
+     * 
+     */
     ros::Timer timer;
+
+    /**
+     * @brief Mutex to protect the timer.
+     * 
+     */
     std::mutex mTimer;
+
+    /**
+     * @brief Used to signal the end of waiting either by elapse of timer or interrupt.
+     * 
+     */
     std::condition_variable cvWait;
+
+    /**
+     * @brief Mutex used for signalling.
+     * 
+     */
     std::mutex mWait;
+    
+    /**
+     * @brief Flag to indicate if the timer was interrupted.
+     * 
+     */
     std::atomic<bool> interrupted;
+
+    /**
+     * @brief Flag to indicate if the timer has elapsed.
+     * 
+     */
     std::atomic<bool> timeout;
     
+    /**
+     * @brief Callback for when the timer elapses.
+     * 
+     * @param event 
+     */
     void timerCallback(const ros::TimerEvent& event)
     {
         std::lock_guard<std::mutex> lock(mWait);
@@ -23,8 +61,23 @@ private:
     }
     
 public:
-    WaitTimer() {}
+    /**
+     * @brief Amount of time (seconds) to wait for.
+     * 
+     */
+    double waitTime;
 
+    /**
+     * @brief Construct a new Wait Timer object.
+     * 
+     */
+    WaitTimer() : waitTime(0) {}
+
+    /**
+     * @brief Construct a new Wait Timer object with a timer that will elapse after the specified waiting time.
+     * 
+     * @param waitTime Amount of time to wait for the timer to elapse.
+     */
     WaitTimer(double waitTime) : waitTime(waitTime)
     {
         interrupted = false;
@@ -33,6 +86,10 @@ public:
         timer = nh.createTimer(ros::Duration(waitTime), &WaitTimer::timerCallback, this, true, false);
     }
 
+    /**
+     * @brief Copy constructor.
+     * 
+     */
     WaitTimer(const WaitTimer& other)
     {
         interrupted = false;
@@ -42,6 +99,10 @@ public:
         timer = nh.createTimer(ros::Duration(waitTime), &WaitTimer::timerCallback, this, true, false);
     }
 
+    /**
+     * @brief Move constructor.
+     * 
+     */
     WaitTimer(const WaitTimer&& other)
     {
         interrupted = false;
@@ -51,6 +112,10 @@ public:
         timer = nh.createTimer(ros::Duration(waitTime), &WaitTimer::timerCallback, this, true, false);
     }
 
+    /**
+     * @brief Copy assignment.
+     * 
+     */
     WaitTimer& operator=(const WaitTimer& other)
     {
         interrupted = false;
@@ -61,6 +126,10 @@ public:
         return *this;
     }
 
+    /**
+     * @brief Move assignment.
+     * 
+     */
     WaitTimer& operator=(const WaitTimer&& other)
     {
         interrupted = false;
@@ -71,9 +140,14 @@ public:
         return *this;
     }
     
+    /**
+     * @brief Begin waiting for the timer to elapse. Does nothing if the timer was not initialized or wait time is zero.
+     * @details This is a blocking call. It will return when either the timer has elapsed or if it was interrupted.
+     * 
+     */
     void wait()
     {
-        if (!timer.isValid())
+        if (!timer.isValid() || waitTime == 0)
         {
             return;
         }
@@ -94,6 +168,10 @@ public:
         );
     }
 
+    /**
+     * @brief Interrupts the timer so it will return from waiting if is already waiting.
+     * 
+     */
     void interrupt()
     {
         if (!timer.isValid() || !timer.hasStarted())
@@ -110,6 +188,12 @@ public:
         cvWait.notify_one();
     }
 
+    /**
+     * @brief Queries if the timer was interrupted since the last waiting call.
+     * 
+     * @return true If the timer was interrupted.
+     * @return false If the timer was not interrupted and has elapsed.
+     */
     bool isInterrupted()
     {
         if (!timer.isValid())
