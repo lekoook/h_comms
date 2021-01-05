0b250c9acd735137a8e21033809cd864d41f7e7f
Tze Guang <idledile@gmail.com>
2021-01-05 20:01:23 +0800

    CMake compilation

---
+++ db_test/src/db_test_node.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <db_client/Db.hpp>
+#include <vector>
+#include <cstdint>
+
+int main(){
+    Db d;
+    Db::Schema s;
+    s.id = 20314;
+    s.timestamp = 22110304;
+    s.data = "This is data";
+
+    std::vector<Db::Schema> v;
+    v.push_back(s);
+
+    std::vector<uint16_t> v1;
+    v1.push_back(20314);
+
+    d.insert(v);
+    d.select(v1);
+}    
