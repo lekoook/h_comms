d5406517081982a13ff83a78517e162ff2986552
lekoook <kokteng1313@gmail.com>
2021-01-09 15:31:47 +0800

    Add methods to serialisers

--- ptp_comms/src/serialisers.cpp
+++ ptp_comms/src/serialisers.cpp
@@ -21,6 +21,18 @@ namespace serialisers
         dest[3] = (uint8_t) (src & 0xff);
     }
 
+    void copyU64(uint8_t* dest, uint64_t src)
+    {
+        dest[0] = (uint8_t) ((src >> 56) & 0xff);
+        dest[1] = (uint8_t) ((src >> 48) & 0xff);
+        dest[2] = (uint8_t) ((src >> 40) & 0xff);
+        dest[3] = (uint8_t) ((src >> 32) & 0xff);
+        dest[4] = (uint8_t) ((src >> 24) & 0xff);
+        dest[5] = (uint8_t) ((src >> 16) & 0xff);
+        dest[6] = (uint8_t) ((src >> 8) & 0xff);
+        dest[7] = (uint8_t) (src & 0xff);
+    }
+
     uint8_t getU8(uint8_t* src)
     {
         return src[0];
@@ -29,18 +41,32 @@ namespace serialisers
     uint16_t getU16(uint8_t* src)
     {
         uint16_t val = 0;
-        val |= (src[0] & 0xff) << 8;
-        val |= (src[1] & 0xff);
+        val |= (uint16_t)src[0] << 8;
+        val |= (uint16_t)src[1];
         return val;
     }
 
     uint32_t getU32(uint8_t* src)
     {
         uint32_t val = 0;
-        val |= (src[0] & 0xff) << 24;
-        val |= (src[1] & 0xff) << 16;
-        val |= (src[2] & 0xff) << 8;
-        val |= (src[3] & 0xff);
+        val |= (uint32_t)src[0] << 24;
+        val |= (uint32_t)src[1] << 16;
+        val |= (uint32_t)src[2] << 8;
+        val |= (uint32_t)src[3];
+        return val;
+    }
+
+    uint64_t getU64(uint8_t* src)
+    {
+        uint64_t val = 0;
+        val |= (uint64_t)src[0] << 56;
+        val |= (uint64_t)src[1] << 48;
+        val |= (uint64_t)src[2] << 40;
+        val |= (uint64_t)src[3] << 32;
+        val |= (uint64_t)src[4] << 24;
+        val |= (uint64_t)src[5] << 16;
+        val |= (uint64_t)src[6] << 8;
+        val |= (uint64_t)src[7];
         return val;
     }
 }
