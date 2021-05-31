207c53778d2c1abbd693ca641d82131554bc7045
zhengqunkoo <root@zhengqunkoo.com>
2021-05-31 23:53:28 +0800

    exchange_graphs: Have each node spawn one robot
    This commit is not needed if the launch file can pass arguments to the node

---
+++ exchange_graphs/src/exchange_graphs_node-X3.cpp
@@ -0,0 +1,9 @@
+#include <ros/ros.h>
+#include <exchange_graphs/ExchangeGraphs.hpp>
+
+int main(int argc, char **argv) {
+    ros::init(argc, argv, "exchange_graphs");
+    exchange_graphs::ExchangeGraphs exchangeGraphs(3, {{"X1", 1}, {"X2", 2}, {"X3", 3}});
+    ros::spin();
+    return 0;
+}
