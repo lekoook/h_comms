c63f28988caba697ba8ce79701d1b97f8a011716
zhengqunkoo <root@zhengqunkoo.com>
2021-06-01 21:44:46 +0800

    exchange_graphs: Launch one ptp_comms and one exchange_graph per roslaunch

--- exchange_graphs/src/exchange_graphs_node-X2.cpp
+++
@@ -1,9 +0,0 @@
-#include <ros/ros.h>
-#include <exchange_graphs/ExchangeGraphs.hpp>
-
-int main(int argc, char **argv) {
-    ros::init(argc, argv, "exchange_graphs");
-    exchange_graphs::ExchangeGraphs exchangeGraphs(2, {{"X1", 1}, {"X2", 2}, {"X3", 3}});
-    ros::spin();
-    return 0;
-}
--- exchange_graphs/src/exchange_graphs_node.cpp
+++ exchange_graphs/src/exchange_graphs_node.cpp
@@ -2,8 +2,9 @@
 #include <exchange_graphs/ExchangeGraphs.hpp>
 
 int main(int argc, char **argv) {
+    exchange_graphs::ROBOT robot = argv[1];
     ros::init(argc, argv, "exchange_graphs");
-    exchange_graphs::ExchangeGraphs exchangeGraphs(1, {{"X1", 1}, {"X2", 2}, {"X3", 3}});
+    exchange_graphs::ExchangeGraphs exchangeGraphs(robot, {{"X1", 1}, {"X2", 2}, {"X3", 3}});
     ros::spin();
     return 0;
 }
