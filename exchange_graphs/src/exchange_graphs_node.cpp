aaa7406174cb740da890d2e42a3ffa189afabab7
zhengqunkoo <root@zhengqunkoo.com>
2021-06-01 22:26:55 +0800

    exchange_graphs: Fix compile error

--- exchange_graphs/src/exchange_graphs_node.cpp
+++ exchange_graphs/src/exchange_graphs_node.cpp
@@ -2,7 +2,7 @@
 #include <exchange_graphs/ExchangeGraphs.hpp>
 
 int main(int argc, char **argv) {
-    exchange_graphs::ROBOT robot = argv[1];
+    exchange_graphs::ROBOT robot = std::atoi(argv[1]);
     ros::init(argc, argv, "exchange_graphs");
     exchange_graphs::ExchangeGraphs exchangeGraphs(robot, {{"X1", 1}, {"X2", 2}, {"X3", 3}});
     ros::spin();
