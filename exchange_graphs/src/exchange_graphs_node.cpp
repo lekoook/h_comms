3624512535ff578d0c7cd040a752dae7edd5435f
zhengqunkoo <root@zhengqunkoo.com>
2021-07-10 19:29:07 +0800

    exchange_graphs: Parse robot name then convert to robot number

--- exchange_graphs/src/exchange_graphs_node.cpp
+++ exchange_graphs/src/exchange_graphs_node.cpp
@@ -2,9 +2,10 @@
 #include <exchange_graphs/ExchangeGraphs.hpp>
 
 int main(int argc, char **argv) {
-    exchange_graphs::ROBOT robot = std::atoi(argv[1]);
     ros::init(argc, argv, "exchange_graphs");
-    exchange_graphs::ExchangeGraphs exchangeGraphs(robot, {{"X1", 1}, {"X2", 2}, {"X3", 3}});
+    exchange_graphs::NEIGHBOR_TO_ROBOT neighborToRobot = {{"X1", 1}, {"X2", 2}, {"X3", 3}};
+    exchange_graphs::ROBOT robot = neighborToRobot[argv[1]];
+    exchange_graphs::ExchangeGraphs exchangeGraphs(robot, neighborToRobot);
     ros::spin();
     return 0;
 }
